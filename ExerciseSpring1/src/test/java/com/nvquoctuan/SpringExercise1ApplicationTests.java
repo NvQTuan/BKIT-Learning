75cc4250af8f1770a50b98b17b90ab352e941940
nvtuanq <nvtuanq321@gmail.com>
2022-01-12 10:30:25 +0700

    Add submodule

---
+++ ExerciseSpring1/src/test/java/com/nvquoctuan/SpringExercise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvquoctuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExercise1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
