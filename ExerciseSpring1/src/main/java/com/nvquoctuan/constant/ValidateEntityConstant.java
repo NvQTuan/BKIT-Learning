0ef1494d60f30b846b96870da704de050197c185
nvtuanq <nvtuanq321@gmail.com>
2022-01-13 11:09:45 +0700

    Add Validate for entity

---
+++ ExerciseSpring1/src/main/java/com/nvquoctuan/constant/ValidateEntityConstant.java
@@ -0,0 +1,9 @@
+package com.nvquoctuan.constant;
+
+public class ValidateEntityConstant {
+
+  public static final int MIN_SIZE_USERNAME = 6;
+  public static final int MAX_SIZE_USERNAME = 64;
+
+  public static final int MAX_SIZE_CHARACTER = 64;
+}
---
+++ ExerciseSpring1/src/main/java/com/nvquoctuan/entity/UserEntity.java
@@ -0,0 +1,53 @@
+package com.nvquoctuan.entity;
+
+import com.nvquoctuan.constant.ValidateEntityConstant;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.validation.constraints.Max;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user_entity")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "user_name")
+  @Size(min = ValidateEntityConstant.MIN_SIZE_USERNAME, max = ValidateEntityConstant.MAX_SIZE_USERNAME)
+  private String userName;
+
+  @Column(name = "last_name")
+  @Max(value = ValidateEntityConstant.MAX_SIZE_CHARACTER)
+  private String lastName;
+
+  @Column(name = "first_name")
+  @Max(value = ValidateEntityConstant.MAX_SIZE_CHARACTER)
+  private String firstname;
+
+  @Column(name = "birth_day")
+  @Temporal(TemporalType.DATE)
+  private Date birthDay;
+
+  @Column(name = "inserted_day")
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date insertedDate;
+}
