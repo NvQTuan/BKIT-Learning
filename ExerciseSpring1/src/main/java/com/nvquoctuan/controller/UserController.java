cfb31fd5757514d19873c368ddf068828b69d340
nvtuanq <nvtuanq321@gmail.com>
2022-01-16 11:30:03 +0700

    Change response all value of user to specific value

--- ExerciseSpring1/src/main/java/com/nvquoctuan/controller/UserController.java
+++ ExerciseSpring1/src/main/java/com/nvquoctuan/controller/UserController.java
@@ -2,20 +2,21 @@ package com.nvquoctuan.controller;
 
 import com.nvquoctuan.constant.PageConstant;
 import com.nvquoctuan.dto.UserDto;
+import com.nvquoctuan.dto.UserResponseDto;
 import com.nvquoctuan.entity.UserEntity;
 import com.nvquoctuan.service.UserServiceImpl;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,28 +28,38 @@ public class UserController {
   private final UserServiceImpl userService;
 
   @GetMapping("/getUser")
-  public List<UserEntity> getByUserNameOrFirstName(@RequestParam(name = "firstName",
+  public List<UserResponseDto> getByUserNameOrFirstName(@RequestParam(name = "firstName",
       defaultValue = "") String firstName, @RequestParam(defaultValue = "1") Integer page,
       @RequestParam(required = false, defaultValue = PageConstant.PAGE_SIZE) Integer size) {
     final List<UserEntity> userEntity = userService.getUserByFirstName(firstName, page, size);
-    return userEntity;
+    return userEntity.stream().map(user -> buildUserResponseDto(user.getUserName()))
+        .collect(Collectors.toList());
   }
 
   @GetMapping("/getUserByFullName")
-  public List<UserEntity> getByUserNameOrFullName(@RequestParam(name = "keyword",
+  public List<UserResponseDto> getByUserNameOrFullName(@RequestParam(name = "keyword",
       defaultValue = "") String keyword) {
     final List<UserEntity> userEntity = userService.getUserByFullName(keyword);
-    return userEntity;
+    return userEntity.stream().map(user -> buildUserResponseDto(user.getUserName()))
+        .collect(Collectors.toList());
   }
 
   @PostMapping
-  public ResponseEntity<?> createUser(@Valid @RequestBody UserDto userDto) {
+  @ResponseStatus(HttpStatus.CREATED)
+  public UserResponseDto createUser(@Valid @RequestBody UserDto userDto) {
     final UserEntity userEntity = UserEntity.builder()
         .userName(userDto.getUserName())
         .firstName(userDto.getFirstName())
         .lastName(userDto.getLastName())
         .birthDay(userDto.getBirthDay())
         .build();
-    return new ResponseEntity<>(userService.createUser(userEntity), HttpStatus.CREATED);
+    final UserEntity responseUserEntity = userService.createUser(userEntity);
+    return buildUserResponseDto(responseUserEntity.getUserName());
+  }
+
+  private UserResponseDto buildUserResponseDto(String userName) {
+    return UserResponseDto.builder()
+        .userName(userName)
+        .build();
   }
 }
